4c95406a6df6c0d5598b99c8cebd885f5350f681
Guilherme <guilherme@alume.com>
2024-01-23 22:06:45 -0300

    change header styles on scroll page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { PropsWithChildren, useState } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import { twMerge } from "tailwind-merge"
 import { MdWhatsapp } from "react-icons/md"
 import { FiInstagram } from "react-icons/fi";
@@ -10,20 +10,35 @@ const Link = ({ children }: PropsWithChildren) => {
 }
 
 export function Header() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   function toggleMenu() {
     setIsOpen(!isOpen);
   }
 
+  function handleScroll() {
+    setHasScrolled(window.scrollY > 0)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', handleScroll);
+    }
+
+    return (() => {
+      window.removeEventListener("scroll", handleScroll);
+    })
+  }, []);
+
   return (
-    <header className="fixed w-full top-0 border-b border-sand bg-transparent z-50">
+    <header className={twMerge("fixed w-full top-0 z-50 transition-all", hasScrolled ? "backdrop-blur-md bg-white/20 shadow-lg" : "bg-transparent")}>
       <div className="container">
-        <nav className="flex justify-between items-center py-5">
+        <nav className="flex justify-between items-center py-4">
           <div className="w-10 h-10 bg-slate-300"></div>
           <button className={twMerge("burger", isOpen && 'open')} onClick={toggleMenu}></button>
 
-          <ul className={twMerge("absolute top-0 left-0 w-full h-screen bg-white z-50 translate-x-0", "flex flex-col items-center gap-10 pt-28 transition-[all_250ms_ease-out]", !isOpen && 'translate-x-full')}>
+          <ul className={twMerge("absolute top-0 left-0 w-full h-screen bg-white z-50 translate-x-0", "flex flex-col items-center gap-10 pt-28 transition-all ease-in-out duration-300", !isOpen && 'translate-x-full')}>
             <Link>Início</Link>
             <Link>Temas</Link>
             <Link>Modalidades</Link>
