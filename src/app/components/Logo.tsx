9218a513be262af747c147d6267735c20c4903fc
Guilherme <guilherme@alume.com>
2024-02-21 22:16:37 -0300

    fix logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,8 +3,8 @@ import { ComponentProps } from "react";
 export function Logo(props: ComponentProps<'svg'>) {
   return (
     <svg viewBox="0 0 100 72" fontFamily="var(--font-seasons-bold)" fill="currentColor" {...props}>
-      <text x="0" y="50" font-size="64">CG</text>
-      <text x="3" y="68" font-size="16">Carol Godoy</text>
+      <text x="0" y="50" fontSize="64">CG</text>
+      <text x="3" y="68" fontSize="16">Carol Godoy</text>
     </svg>
   )
 }
