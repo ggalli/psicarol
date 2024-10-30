b37ff6932142fa1f5046d2265511fa49a329869d
Guilherme <guilherme@alume.com>
2024-10-29 21:19:44 -0300

    change html lang to pt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={twMerge(theSeasons.variable, roboto.variable)}>
+    <html lang="pt" className={twMerge(theSeasons.variable, roboto.variable)}>
       <body className={roboto.className}>{children}</body>
       <GoogleTagManager gtmId="GTM-KMZMVLTK" />
     </html>
